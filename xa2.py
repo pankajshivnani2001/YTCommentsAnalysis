b93dd16e7e7ad1135832b9ce7e5f0894e2eeb1d3
pankajshivnani2001 <67276848+pankajshivnani2001@users.noreply.github.com>
2022-01-26 11:40:51 +0530

    Update xa2.py

--- xa2.py
+++ xa2.py
@@ -28,59 +28,55 @@ import preprocessing
 import visualizer
 
 def app():
-    try:
-        st.title("Comments Visualizations")
-        st.subheader("Upload the comments file:")
-        st.write("Or Scrape by using the Scrapping Module")
+    st.title("Comments Visualizations")
+    st.subheader("Upload the comments file:")
+    st.write("Or Scrape by using the Scrapping Module")
 
-        uploaded_file = st.file_uploader("Choose a file")
-        df = pd.read_csv(uploaded_file)
-        df.drop(columns=df.columns[0],
-                axis=1,
-                inplace=True)
-        st.write("Original DataFrame")
-        st.write(df)
-
-
-        #url = st.text_input("YouTube URL", placeholder="YouTube URL")
-        #df = commentScrapper.scrape_youtube_comments(url)
-
-        df = preprocessing.preprocessing(df)
-        st.write("Processed DataFrame")
-        st.write(df)
-
-        st.subheader("Word Cloud")
-        st.pyplot(visualizer.createCloud(df["Comment"], "Comments Word Cloud"))
-
-        st.subheader("Top 10 Unigrams")
-        st.plotly_chart(visualizer.plot_word_freq_chart(visualizer.top_n_words(df["Comment"], 1, 10)))
-
-        st.subheader("Top 10 Bigrams")
-        st.plotly_chart(visualizer.plot_word_freq_chart(visualizer.top_n_words(df["Comment"], 2, 10)))
-
-        st.subheader("Top 10 Trigrams")
-        st.plotly_chart(visualizer.plot_word_freq_chart(visualizer.top_n_words(df["Comment"], 3, 10)))
-
-        st.subheader("Part-Of-Speech(POS) Tags")
-        st.plotly_chart(visualizer.plot_tag_count(df))
-
-        st.subheader("Top 10 words in the most liked comments")
-        visualizer.common_words_in_most_liked_comments(df)
-
-        st.subheader("Top 10 words in the least liked comments")
-        visualizer.common_words_in_least_liked_comments(df)
-
-        st.subheader("Box Plot about the length of the Comments")
-        st.plotly_chart(visualizer.length_box_plot(df))
-
-        st.subheader("Emojis and their Counts")
-        st.plotly_chart(visualizer.plot_emoji_count(df))
-
-        st.subheader("Top Emojis")
-        st.plotly_chart(visualizer.emoji_pie_chart(df))
-
-    except:
-        pass
+    uploaded_file = st.file_uploader("Choose a file")
+    df = pd.read_csv(uploaded_file)
+    df.drop(columns=df.columns[0],
+            axis=1,
+            inplace=True)
+    st.write("Original DataFrame")
+    st.write(df)
 
 
-app()
+    #url = st.text_input("YouTube URL", placeholder="YouTube URL")
+    #df = commentScrapper.scrape_youtube_comments(url)
+
+    df = preprocessing.preprocessing(df)
+    st.write("Processed DataFrame")
+    st.write(df)
+
+    st.subheader("Word Cloud")
+    st.pyplot(visualizer.createCloud(df["Comment"], "Comments Word Cloud"))
+
+    st.subheader("Top 10 Unigrams")
+    st.plotly_chart(visualizer.plot_word_freq_chart(visualizer.top_n_words(df["Comment"], 1, 10)))
+
+    st.subheader("Top 10 Bigrams")
+    st.plotly_chart(visualizer.plot_word_freq_chart(visualizer.top_n_words(df["Comment"], 2, 10)))
+
+    st.subheader("Top 10 Trigrams")
+    st.plotly_chart(visualizer.plot_word_freq_chart(visualizer.top_n_words(df["Comment"], 3, 10)))
+
+    st.subheader("Part-Of-Speech(POS) Tags")
+    st.plotly_chart(visualizer.plot_tag_count(df))
+
+    st.subheader("Top 10 words in the most liked comments")
+    visualizer.common_words_in_most_liked_comments(df)
+
+    st.subheader("Top 10 words in the least liked comments")
+    visualizer.common_words_in_least_liked_comments(df)
+
+    st.subheader("Box Plot about the length of the Comments")
+    st.plotly_chart(visualizer.length_box_plot(df))
+
+    st.subheader("Emojis and their Counts")
+    st.plotly_chart(visualizer.plot_emoji_count(df))
+
+    st.subheader("Top Emojis")
+    st.plotly_chart(visualizer.emoji_pie_chart(df))
+
+
+#app()
