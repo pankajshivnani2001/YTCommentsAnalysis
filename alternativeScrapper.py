b66b1735778628100e2aa196bcbe207de6e5f6ec
pankajshivnani2001 <67276848+pankajshivnani2001@users.noreply.github.com>
2022-02-04 09:22:01 +0530

    Update alternativeScrapper.py

--- alternativeScrapper.py
+++ alternativeScrapper.py
@@ -4,17 +4,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import streamlit as st
 
+def scrape(url):
+    data=[]
 
-  data=[]
+    with Chrome(executable_path=r'C:\Program Files\chromedriver.exe') as driver:
+        wait = WebDriverWait(driver,15)
+        driver.get(url)
 
-  with Chrome(executable_path=r'C:\Program Files\chromedriver.exe') as driver:
-      wait = WebDriverWait(driver,15)
-      driver.get("https://www.youtube.com/watch?v=kuhhT_cBtFU&t=2s")
+        for item in range(200): 
+            wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
+            time.sleep(15)
 
-      for item in range(200): 
-          wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-          time.sleep(15)
+        for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
+            data.append(comment.text)
+            
+    return data
+  
+  def app():
+    url = st.text_input("YouTube URL", placeholder = "Enter YouTube URL")
+    data = scrape(url)
+    st.write(data)
+    
 
-      for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
-          data.append(comment.text)
+app()
