377260b2b6657abd4b8b2d864a6d3e7bdb4ff9d6
pankajshivnani2001 <67276848+pankajshivnani2001@users.noreply.github.com>
2022-02-04 12:20:07 +0530

    Update alternativeScrapper.py

--- alternativeScrapper.py
+++ alternativeScrapper.py
@@ -10,6 +10,13 @@ def scrape(url):
     data=[]
 
     with Chrome(executable_path=r'C:\Program Files\chromedriver.exe') as driver:
+        option = webdriver.ChromeOptions()
+        option.add_argument("--headless")  # to make sure that the GUI is not visible to the user
+        option.add_argument("--disable-dev-shm-usage")
+        option.add_argument("--no-sandbox")
+        # change executable path according to the web driver location on your system
+        option.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=option)
         wait = WebDriverWait(driver,15)
         driver.get(url)
 
